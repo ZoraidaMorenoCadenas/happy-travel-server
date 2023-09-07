8ca38b72bea1fadb15777f06fc5dddad4ce85209
Sylvia Llorente <sylviall81@gmail.com>
2023-09-07 06:41:47 +0200

    routes api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+// Public routes
+
+//users
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//Destinations
+Route::get('/destinations', [DestinationController::class, 'index']);
+Route::get('/destinations/{id}', [DestinationController::class, 'show']); //hay q protegerla¿?
+Route::get('/destinations/search/{name}', [DestinationController::class, 'search']);
+
+// Protected routes
+
+// //Route::group(['middleware' => ['auth:sanctum']], function () {
+//     Route::post('/destinations', [DestinationController::class, 'store']);
+//     Route::put('/destinations/{id}', [DestinationController::class, 'update']);
+//     Route::delete('/destinations/{id}', [DestinationController::class, 'destroy']);
+//     Route::post('/logout', [AuthController::class, 'logout']);
+// });
+
+//deberian ser protegidas Destination
+Route::post('/destinations', [DestinationController::class, 'store']);
+    Route::post('/destinations/{id}', [DestinationController::class, 'update']);
+    Route::delete('/destinations/{id}', [DestinationController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
